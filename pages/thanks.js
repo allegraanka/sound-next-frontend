65d28b99da78b81286ec3235701604689434c6bf
Allegra Anka <allegraanka@Allegras-MacBook-Pro.local>
2022-03-04 05:46:08 -0500

    removed old styles modules, caused build error

--- pages/thanks.js
+++ pages/thanks.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const Thanks = () => {
     return(
-        <div className={styles.container}>
+        <div className={``}>
             <span className={`text-9xl text-red-light uppercase`}>Thank you</span>
-            <span className={styles.subCopy}>We&#39;re excited to build community with you.</span>
-            <span className={styles.subReturn}>You&apos;re all set. You can return <Link href='/'><a>home</a></Link> or close this window.</span>
+            <span className={``}>We&#39;re excited to build community with you.</span>
+            <span className={``}>You&apos;re all set. You can return <Link href='/'><a>home</a></Link> or close this window.</span>
         </div>
     );
 }
