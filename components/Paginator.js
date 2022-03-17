fff98e2f4a0a3a698960b52db20bdbbf0706051c
Allegra Anka <allegraanka@Allegras-MacBook-Pro.local>
2022-03-16 22:24:05 -0400

    added friends of the sound and bug jar logo into footer

--- components/Paginator.js
+++ components/Paginator.js
@@ -1,51 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import ShowComponent from './ShowComponent';
+import { useState, useEffect } from 'react';
+import ShowComponent from "./ShowComponent";
 
-function Items({ currentItems }) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <ShowComponent key={item.id} show={item}/>
-        ))}
-    </>
-  );
-}
+export default function Paginator({ items, itemsPerPage, pageLimit }) {
+  const [pages] = useState(Math.ceil(items.length / itemsPerPage));
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default function PaginatedItems({ items, itemsPerPage }) {
-  const [currentItems, setCurrentItems] = useState(null);
-  const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0);
+  function getNextPage() {
+    setCurrentPage((page) => page + 1);
+  }
 
-  useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    setCurrentItems(items.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(items.length / itemsPerPage));
-  }, [items, itemOffset, itemsPerPage]);
+  function getPrevPage() {
+    setCurrentPage((page) => page - 1);
+  }
 
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
+  function changePage(event) {
+    const pageNumber = Number(event.target.textContent);
+    setCurrentPage(pageNumber);
+  }
+
+  const getPaginatedItems = () => {
+    const startIndex = currentPage * pageLimit - pageLimit;
+    const endIndex = startIndex + pageLimit;
+    console.log('what is ittttttt', items.slice(startIndex, endIndex));
+    return items.slice(startIndex, endIndex);
+  }
 
   return (
-    <>
-      <Items currentItems={currentItems} />
-      <ReactPaginate
-        className={`inline`}
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
-    </>
+    <div>
+
+      {currentItems && currentItems.map((show) => (
+         <ShowComponent key={show.id} show={show}/>
+      ))}
+
+      <div className={`flex justify-between`}>
+        <div className={`flex space-x-1`}>
+          <div className={`bg-black text-white w-fit px-4 py-2`}>
+            FIRST
+          </div>
+          <div 
+            className={`bg-black text-white w-fit px-4 py-2 ${currentPage === 1 ? 'disabled' : ''}`}
+            onClick={getPrevPage}>
+            PREV
+          </div>
+        </div>
+
+        <div className={`flex space-x-1`}>
+          <div 
+            className={`bg-black text-white w-fit px-4 py-2 ${currentPage === pages ? 'disabled' : ''}`}
+            onClick={getNextPage}>
+            NEXT
+          </div>
+          <div className={`bg-black text-white w-fit px-4 py-2`}>
+            LAST
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
