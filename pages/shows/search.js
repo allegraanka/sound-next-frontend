bab7edae659b4827321022c1d7649deb97980601
Allegra Anka <allegra.anka@piano.io>
2022-07-10 15:47:14 -0400

    comment out search component that queried strapi which is no longer used

--- pages/shows/search.js
+++ pages/shows/search.js
@@ -1,54 +1,31 @@
-import Layout from '../../components/Layout';
-import ShowComponent from '../../components/ShowComponent';
-import { useRouter } from 'next/router';
-import { fetchAPI } from '../../lib/api';
+// import Layout from '../../components/Layout';
+// import ShowComponent from '../../components/ShowComponent';
+// import { useRouter } from 'next/router';
 
-export async function getServerSideProps({ query: {term}}) {
-    const shows = await fetchAPI('/shows', {
-        filters: {
-            $or: [
-                {
-                    headliner: {
-                        $containsi: term
-                    },
-                },
-                {
-                    support: {
-                        $containsi: term
-                    },
-                },
-                {
-                    venue: {
-                        $contains: term
-                    },
-                },
-            ]
-        }
-    })
-  
-    return {
-      props: {
-        shows: shows.data
-      }
-    }
-  }
+// export async function getServerSideProps({ query: { term } }) {
+//     return {
+//         props: {
+//             shows: shows.data
+//         }
+//     }
+// }
 
-const SearchPage = ({ shows }) => {
-    const router = useRouter();
+// const SearchPage = ({ shows }) => {
+//     const router = useRouter();
 
-    return(
-        <Layout title='The Sound | Upcoming Shows'>
-            <div className={`px-4`}>
-                <div className={``}>
-                    <h1 className={`text-5xl`}>Search results for <span className={`text-red-dark`}>{router.query.term}</span></h1>
-                    {shows.length === 0 && <p>Sorry! Nothing matched that search.</p>}
-                    {shows.map((show) => (
-                        <ShowComponent key={show.id} show={show}/>
-                    ))}
-                </div>
-            </div>
-        </Layout>  
-    );
-}
+//     return (
+//         <Layout title='The Sound | Upcoming Shows'>
+//             <div className={`px-4`}>
+//                 <div className={``}>
+//                     <h1 className={`text-5xl`}>Search results for <span className={`text-red-dark`}>{router.query.term}</span></h1>
+//                     {shows.length === 0 && <p>Sorry! Nothing matched that search.</p>}
+//                     {shows.map((show) => (
+//                         <ShowComponent key={show.id} show={show} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </Layout>
+//     );
+// }
 
-export default SearchPage;
+// export default SearchPage;
